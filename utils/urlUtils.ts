235af118a71681c66c867cf78e0298b48219659d
ngalhotra-dot <ngalhotra@deloitte.com>
2026-09-08 17:59:38 +0530

    Delete utils/urlUtils.ts

--- utils/urlUtils.ts
+++
@@ -1,3 +0,0 @@
-export function getOrderDetailsUrl(orderId: string): string {
-  return `https://oemsalesaftersalesorg--scaletest.sandbox.lightning.force.com/lightning/r/WorkOrder/${orderId}/view`;
-}
