4599d55feffe756672cccdc0e5f6a569a2003e94
Neha Galhotra <ngalhotra@deloitte.com>
2026-09-09 15:56:42 +0530

    email validation

---
+++ test-data/studentdata2.ts
@@ -0,0 +1,28 @@
+export const studentdata2 = [
+    {
+        firstName: 'Neha',
+        lastName: 'Tester',
+        email: 'neha.tester@example.com',
+        mobile: '9876543210',
+        address: 'Gurgaon',
+        gender: 'Female' as const,
+        hobby: 'Sports' as const,
+        state: 'NCR',
+        city: 'Delhi',
+        dob: '01 Jan 1990',
+        subjects: 'Maths' as const
+    },
+    {
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'john.doe',
+        mobile: '9876543211',
+        address: 'Mumbai',
+        gender: 'Male' as const,
+        hobby: 'Reading' as const,
+        state: 'NCR',
+        city: 'Delhi',
+        dob: '01 Jan 1991',
+        subjects: 'Maths' as const
+    }
+]
--- tests/practice-form.spec.ts
+++ tests/practice-form.spec.ts
@@ -1,35 +1,43 @@
 import { test, expect } from '@playwright/test';
 import { homePageLocators } from '../locators/homePageLocators';
 import { practiceFormPage } from '../locators/PracticeFormPage';
-import { studentData } from '../test-data/studentData';
-test('Successful Registration', async ({ page }) => {
+import { studentdata2 } from '../test-data/studentdata2';
+test.only('Successful Registration', async ({ page }) => {
     await page.goto('/forms');
     await homePageLocators.practiceForm(page).click();
     await expect(practiceFormPage.formHeading(page)
     ).toBeVisible();
-    await practiceFormPage.firstName(page).fill(studentData.firstName);
-    await practiceFormPage.lastName(page).fill(studentData.lastName);
-    await practiceFormPage.email(page).fill(studentData.email);
-    await practiceFormPage.selectGender(page, studentData.gender);
-    await practiceFormPage.mobile(page).fill(studentData.mobile);
-    await practiceFormPage.selectHobby(page, studentData.hobby);
-    await practiceFormPage.selectState(studentData.state, page);
-    await practiceFormPage.selectCity(studentData.city, page);
-    await practiceFormPage.currentAddress(page).fill(studentData.address);
+    await practiceFormPage.firstName(page).fill(studentdata2[1].firstName);
+    await practiceFormPage.lastName(page).fill(studentdata2[1].lastName);
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const defaultEmail = 'test@example.com';
+    const emailToEnter = emailRegex.test(studentdata2[1].email)
+        ? studentdata2[1].email
+        : defaultEmail;
+
+    await practiceFormPage.email(page).fill(emailToEnter);
+    console.log('Email entered:', emailToEnter);
+    await practiceFormPage.selectGender(page, studentdata2[1].gender);
+    await practiceFormPage.mobile(page).fill(studentdata2[1].mobile);
+    await practiceFormPage.selectHobby(page, studentdata2[1].hobby);
+    await practiceFormPage.selectState(studentdata2[1].state, page);
+    await practiceFormPage.selectCity(studentdata2[1].city, page);
+    await practiceFormPage.currentAddress(page).fill(studentdata2[1].address);
     await practiceFormPage.dob(page).click();
-    await practiceFormPage.dob(page).fill(studentData.dob);
+    await practiceFormPage.dob(page).fill(studentdata2[1].dob);
     await practiceFormPage.dob(page).press('Enter');
     await practiceFormPage.subjects(page).click();
-    await practiceFormPage.subjects(page).fill(studentData.subjects);
+    await practiceFormPage.subjects(page).fill(studentdata2[1].subjects);
     await practiceFormPage.subjects(page).press('Enter');
     await practiceFormPage.submitButton(page).click();
     await practiceFormPage.verifySubmission(page);
-    await expect(practiceFormPage.verifyDetails(page)).toContainText(studentData.firstName);
-    await expect(practiceFormPage.verifyDetails(page)).toContainText(studentData.lastName);
-    await expect(practiceFormPage.verifyDetails(page)).toContainText(studentData.email);
-    await expect(practiceFormPage.verifyDetails(page)).toContainText(studentData.mobile);
-    await expect(practiceFormPage.verifyDetails(page)).toContainText(studentData.address);
-    await expect(practiceFormPage.verifyDetails(page)).toContainText(studentData.subjects);
+    await expect(practiceFormPage.verifyDetails(page)).toContainText(studentdata2[1].firstName);
+    await expect(practiceFormPage.verifyDetails(page)).toContainText(studentdata2[1].lastName);
+    await expect(practiceFormPage.verifyDetails(page)).toContainText(emailToEnter);
+    await expect(practiceFormPage.verifyDetails(page)).toContainText(studentdata2[1].mobile);
+    await expect(practiceFormPage.verifyDetails(page)).toContainText(studentdata2[1].address);
+    await expect(practiceFormPage.verifyDetails(page)).toContainText(studentdata2[1].subjects);
+
 });
 
 test('Should display validation when mandatory fields are missing', async ({ page }) => {
@@ -47,4 +55,5 @@ test('Should display validation when mandatory fields are missing', async ({ pag
             name: 'Student Registration Form'
         })
     ).toBeVisible();
+    await page.waitForTimeout(5000);
 });
